c5da2245f597bc740c475f51f5d3a44eb0fd3143
Mackaber Witckin <mkbravo555@gmail.com>
2023-09-14 21:14:59 -0500

    Ejemplo de moment.js

--- clase-2/main.js
+++ clase-2/main.js
@@ -1,3 +1,8 @@
-let _ = require("underscore")
+const moment = require("moment")
 
-_.list()
+console.log("Cuantos días faltan para navidad?")
+let christmas = moment('2023-12-25T00:00:00Z', moment.ISO_8601);
+let today = moment()
+const days_left = christmas.diff(today, 'days');
+
+console.log(days_left);
