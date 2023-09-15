f31e43147e4001ec7fdd1c3dd43c2ce8ca24d1a6
Mackaber Witckin <mkbravo555@gmail.com>
2023-09-14 21:33:58 -0500

    add start script on package.json

--- clase-2/main.js
+++ clase-2/main.js
@@ -1,8 +1,9 @@
 const moment = require("moment")
 
-console.log("Cuantos días faltan para navidad?")
-let christmas = moment('2023-12-25T00:00:00Z', moment.ISO_8601);
+console.log("Cuantos días faltan para mi cumpleaños?")
+//let christmas = moment('2023-12-25T00:00:00Z', moment.ISO_8601);
+let birthday = moment('2025-12-18T00:00:00Z', moment.ISO_8601);
 let today = moment()
-const days_left = christmas.diff(today, 'days');
+const days_left = birthday.diff(today, 'days');
 
 console.log(days_left);
