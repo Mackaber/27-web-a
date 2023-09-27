99a65b1024c621a2e8fa1c7678c0714da59d41d5
donCelis <alexcelis019@gmail.com>
2023-09-26 22:08:16 -0500

    crear elementos html con document.createElement

--- clase4/main.js
+++ clase4/main.js
@@ -1,29 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import { baseUrl } from "./src/config";
-import { UserCardTemplate } from "./src/components/UserCard";
+import { UserCard, UserCardTemplate } from "./src/components/UserCard";
 
 const usersDom = document.querySelector("#usersDom");
 
 window.addEventListener("DOMContentLoaded", () => {
-  const urlUsers = baseUrl({ path: "/users", limit: 8 });
+  const urlUsers = baseUrl({ path: "/users", limit: 4 });
 
   fetch(urlUsers)
     .then((response) => {
-      // throw new Error(response.status);
       if (!response.ok) {
         throw new Error(response.status);
       }
       return response.json();
     })
     .then(({ users, total, skip, limit }) => {
-      /* usersDom.innerHTML = UserCardTemplate({
-        image: users[0].image,
-        company: { department: users[0].company.department },
-        firstName: users[0].firstName,
-        lastName: users[0].lastName,
+      users.map((user) => usersDom.appendChild(UserCard(user)));
+      // usersDom.appendChild(UserCard(users[0])); -> Template String
+
+      /* const btnLike = document.querySelector("#btnLike");
+      btnLike.addEventListener("click", () => {
+        console.log("click");
       }); */
-      /* usersDom.innerHTML = UserCardTemplate(users[0]); */
-      users.map((user) => (usersDom.innerHTML += UserCardTemplate(user)));
     });
 });
--- clase4/src/components/UserCard.js
+++ clase4/src/components/UserCard.js
@@ -1,4 +1,41 @@
-function UserCard() {}
+function createTags(array) {
+  return array.map(({ tagName, classN }) => {
+    const item = document.createElement(tagName);
+    item.className = classN;
+
+    return item;
+  });
+}
+
+export function UserCard({
+  image,
+  firstName = "",
+  lastName = "",
+  company: { department = "" },
+}) {
+  const tags = [
+    { tagName: "aside", classN: "col-md-3" },
+    { tagName: "div", classN: "card" },
+    { tagName: "img", classN: "card-img-top" },
+    { tagName: "div", classN: "card-body" },
+    { tagName: "h5", classN: "card-title" },
+    { tagName: "p", classN: "card-text" },
+  ];
+
+  const [aside, divCard, img, divBody, h5, p] = createTags(tags);
+
+  img.src = image;
+  h5.textContent = `${firstName} ${lastName}`;
+  p.textContent = department;
+
+  aside.appendChild(divCard);
+  divCard.appendChild(img);
+  divCard.appendChild(divBody);
+  divBody.appendChild(h5);
+  divBody.appendChild(p);
+
+  return aside;
+}
 
 export function UserCardTemplate({
   image,
@@ -17,6 +54,7 @@ export function UserCardTemplate({
       <div class="card-body">
         <h5 class="card-title">${firstName} ${lastName}</h5>
         <p class="card-text">${department}</p>
+        <button id="btnLike" class="btn btn-success">Like</button>
       </div>
     </div>
   </aside>
