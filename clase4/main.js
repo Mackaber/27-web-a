f8f21fb6c0a89b5bae7f7bc453e5aa05823f8580
donCelis <alexcelis019@gmail.com>
2023-09-26 20:15:19 -0500

    primer fetch de los usuarios

--- clase4/main.js
+++ clase4/main.js
@@ -1,4 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
+import { baseUrl } from "./src/config";
 
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Sitio web cargado");
 
+  fetch(baseUrl)
+    .then((response) => {
+      const { ok, status } = response;
+      if (!ok) {
+        throw new Error(status);
+      }
+      return response.json();
+    })
+    .then(({ users, total, skip, limit }) => {
+      
+    });
+});
