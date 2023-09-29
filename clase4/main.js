0dc34a3b864bbb4fea57f2a9a65978859a49ab79
donCelis <alexcelis019@gmail.com>
2023-09-28 21:46:41 -0500

    lista de paises ok

--- clase4/main.js
+++ clase4/main.js
@@ -1,8 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
+import { endpointPaises } from "./src/config";
+import { orderBy } from "lodash";
 
 const app = document.getElementById("app");
+const form = app.querySelector("form");
+const selectDom = app.querySelector(".select-dinamico");
 
+// TODO: Cargar los paises en el select
+// TODO: Buscar desde la api del clima
 window.addEventListener("DOMContentLoaded", () => {
+  selectDom.innerHTML = "<option disabled selected>Selecciona un pais</option>";
+  fetch(endpointPaises)
+    .then((data) => data.json())
+    .then((countries) => {
+      const orderByNameCountries = orderBy(countries, ["name.common", ["asc"]]);
 
+      const option = ({ cca2, name }) => `
+        <option value="${cca2}">${name.common}</option>
+      `;
+
+      orderByNameCountries.map(
+        (country) => (selectDom.innerHTML += option(country))
+      );
+    });
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 });
--- clase4/src/config.js
+++ clase4/src/config.js
@@ -1,3 +1,11 @@
 export const endpoint = "https://dummyjson.com";
 
 export const baseUrl = ({ path, limit }) => `${endpoint}${path}?limit=${limit}`;
+
+const apiKey = "2dc00dcb68b5a8e266c25c184e354660";
+
+export const endpointClima = ({ ciudad, pais }) => {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&lang=sp&units=metric&appid=${apiKey}`;
+};
+
+export const endpointPaises = "https://restcountries.com/v3.1/region/america";
